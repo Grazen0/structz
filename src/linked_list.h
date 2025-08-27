b2aa93e762bdb7de06ad7d11d6a38f9bc68b6210
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-27 02:20:19 -0500

    feat: almost finish linked list

--- src/linked_list.h
+++ src/linked_list.h
@@ -44,35 +44,49 @@ public:
     }
 
     ~LinkedList() {
+        clear();
+    }
+
+    LinkedList<T>& operator=(const LinkedList<T>& other) = delete;
+
+    LinkedList<T>& operator=(LinkedList<T>&& other) noexcept = delete;
+
+    [[nodiscard]] T& front() {
+        if (m_head == nullptr)
+            throw std::runtime_error("list is empty");
+
+        return m_head->data;
+    }
+
+    [[nodiscard]] const T& front() const {
+        if (m_head == nullptr)
+            throw std::runtime_error("list is empty");
+
+        return m_head->data;
+    }
+
+    [[nodiscard]] T& back() {
+        if (m_head == nullptr)
+            throw std::runtime_error("list is empty");
+
         Node* cur = m_head;
 
-        while (cur != nullptr) {
-            Node* const next = cur->next;
-            delete cur;
-            cur = next;
-        }
+        while (cur->next != nullptr)
+            cur = cur->next;
+
+        return cur->data;
     }
 
-    T& front() {
+    [[nodiscard]] const T& back() const {
         if (m_head == nullptr)
             throw std::runtime_error("list is empty");
 
-        return m_head->data;
-    }
+        Node* cur = m_head;
 
-    const T& front() const {
-        if (m_head == nullptr)
-            throw std::runtime_error("list is empty");
+        while (cur->next != nullptr)
+            cur = cur->next;
 
-        return m_head->data;
-    }
-
-    [[nodiscard]] size_t size() const {
-        return m_size;
-    }
-
-    [[nodiscard]] bool empty() const {
-        return m_size == 0;
+        return cur->data;
     }
 
     void push_front(T data) {
@@ -80,13 +94,50 @@ public:
         ++m_size;
     }
 
-    // TODO: implement
-    LinkedList<T>& operator=(LinkedList<T> other) = delete;
+    void push_back(T data) {
+        Node** cur = &m_head;
 
-    // TODO: implement
-    LinkedList<T>& operator=(LinkedList<T>&& other) noexcept = delete;
+        while (*cur != nullptr)
+            cur = &(*cur)->next;
 
-    T const& operator[](const size_t index) const {
+        *cur = new Node(std::move(data));
+        ++m_size;
+    }
+
+    T pop_front() {
+        if (m_head == nullptr)
+            throw std::runtime_error("list is empty");
+
+        const T data = std::move(m_head->data);
+
+        Node* const prev_head = m_head;
+        m_head = m_head->next;
+        delete prev_head;
+
+        --m_size;
+        return data;
+    }
+
+    T pop_back() {
+        if (m_head == nullptr)
+            throw std::runtime_error("list is empty");
+
+        Node** cur = &m_head;
+
+        while ((*cur)->next != nullptr)
+            cur = &(*cur)->next;
+
+        const T data = std::move((*cur)->data);
+
+        Node* prev_tail = *cur;
+        delete prev_tail;
+        *cur = nullptr;
+
+        --m_size;
+        return data;
+    }
+
+    [[nodiscard]] const T& operator[](const size_t index) const {
         if (index >= m_size)
             throw std::out_of_range("list index out of bounds");
 
@@ -98,7 +149,7 @@ public:
         return cur->data;
     }
 
-    T& operator[](const size_t index) {
+    [[nodiscard]] T& operator[](const size_t index) {
         if (index >= m_size)
             throw std::out_of_range("list index out of bounds");
 
@@ -108,6 +159,50 @@ public:
             cur = cur->next;
 
         return cur->data;
+    }
+
+    [[nodiscard]] bool empty() const {
+        return m_size == 0;
+    }
+
+    [[nodiscard]] size_t size() const {
+        return m_size;
+    }
+
+    void clear() {
+        Node* cur = m_head;
+        m_head = nullptr;
+
+        while (cur != nullptr) {
+            Node* const next = cur->next;
+            delete cur;
+            cur = next;
+        }
+
+        m_size = 0;
+    }
+
+    void sort() {
+        // TODO: implement
+    }
+
+    void reverse() {
+        if (m_size <= 1)
+            return;
+
+        Node* cur_first = m_head;
+        Node* cur_second = m_head->next;
+
+        while (cur_second != nullptr) {
+            Node* const next = cur_second->next;
+            cur_second->next = cur_first;
+
+            cur_first = cur_second;
+            cur_second = next;
+        }
+
+        m_head->next = nullptr;
+        m_head = cur_first;
     }
 };
 
--- tests/test_linked_list.cpp
+++ tests/test_linked_list.cpp
@@ -37,14 +37,61 @@ TEST_CASE("linked lists can be copied", "[linked_list]") {
     REQUIRE(list_b[4] == 1);
 }
 
-TEST_CASE("linked lists can be pushed from to the front", "[linked_list]") {
+TEST_CASE("can perform multiple operations on a linked list", "[linked_list]") {
     LinkedList<int> list;
-    list.push_front(1);
-    list.push_front(2);
-    list.push_front(3);
+
+    REQUIRE(list.size() == 0);
+    REQUIRE(list.empty());
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
 
     REQUIRE(list.size() == 3);
+    REQUIRE(list[0] == 1);
+    REQUIRE(list[1] == 2);
+    REQUIRE(list[2] == 3);
+    REQUIRE_THROWS_AS(list[3], std::out_of_range);
+    REQUIRE_THROWS_AS(list[42], std::out_of_range);
+    REQUIRE_THROWS_AS(list[-1], std::out_of_range);
+    REQUIRE(list.front() == 1);
+    REQUIRE(list.back() == 3);
+
+    list.push_front(0);
+
+    REQUIRE(list.size() == 4);
+    REQUIRE(list[0] == 0);
+    REQUIRE(list[1] == 1);
+    REQUIRE(list[2] == 2);
+    REQUIRE(list[3] == 3);
+    REQUIRE(list.front() == 0);
+    REQUIRE(list.back() == 3);
+
+    list.reverse();
+
+    REQUIRE(list.size() == 4);
     REQUIRE(list[0] == 3);
     REQUIRE(list[1] == 2);
     REQUIRE(list[2] == 1);
+    REQUIRE(list[3] == 0);
+    REQUIRE(list.front() == 3);
+    REQUIRE(list.back() == 0);
+
+    const int first = list.pop_front();
+    const int last = list.pop_back();
+    const int prev_last = list.pop_back();
+
+    REQUIRE(list.size() == 1);
+    REQUIRE(list[0] == 2);
+    REQUIRE(first == 3);
+    REQUIRE(prev_last == 1);
+    REQUIRE(last == 0);
+
+    list.clear();
+
+    REQUIRE(list.empty());
+
+    list.push_back(42);
+
+    REQUIRE(list[0] == 42);
 }
