c8d90a55d22d2d5b344cd3439736b3610c6eeede
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-02 23:30:47 -0500

    feat(btree): finish implementing removal

--- include/btree.h
+++ include/btree.h
@@ -5,8 +5,10 @@
 #include <array>
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <initializer_list>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -30,25 +32,8 @@ class BTree {
     struct Node {
         std::array<Entry, M - 1> entries{};
         std::array<Node*, M> children{nullptr};
-        std::size_t entry_count = 0;
+        std::size_t size = 0;
         bool is_leaf = true;
-
-        Node() = default;
-
-        Node(std::initializer_list<std::pair<K, T>> elements)
-            : entry_count(elements.size()) {
-            std::size_t i = 0;
-
-            for (auto& [key, value] : elements) {
-                entries[i] = Entry(key, value);
-                ++i;
-            }
-        }
-
-        void link_child(const std::size_t idx, std::initializer_list<std::pair<K, T>> elements) {
-            is_leaf = false;
-            children[idx] = new Node(elements);
-        }
     };
 
     std::size_t m_size = 0;
@@ -62,10 +47,10 @@ class BTree {
         while (cur != nullptr) {
             std::size_t i = 0;
 
-            while (i < cur->entry_count && self.cmp(cur->entries[i].key, key))
+            while (i < cur->size && self.cmp(cur->entries[i].key, key))
                 ++i;
 
-            if (i < cur->entry_count && cur->entries[i].key == key)
+            if (i < cur->size && cur->entries[i].key == key)
                 return cur->entries[i].value;
 
             if (cur->is_leaf)
@@ -75,6 +60,15 @@ class BTree {
         }
 
         throw std::runtime_error("Key not found");
+    }
+
+    [[nodiscard]] static const Entry& min_entry_by_key(const Node* const node) {
+        const Node* cur = node;
+
+        while (!cur->is_leaf)
+            cur = cur->children[0];
+
+        return cur->entries[0];
     }
 
     [[nodiscard]] static const K& min_key(const Node* const node) {
@@ -90,9 +84,9 @@ class BTree {
         const Node* cur = node;
 
         while (!cur->is_leaf)
-            cur = cur->children[cur->entry_count];
+            cur = cur->children[cur->size];
 
-        return cur->entries[cur->entry_count - 1].key;
+        return cur->entries[cur->size - 1].key;
     }
 
     static std::string to_string(const Node* const node, const std::string& sep = ",") {
@@ -101,18 +95,18 @@ class BTree {
 
         std::string result;
 
-        for (std::size_t i = 0; i < node->entry_count; ++i) {
+        for (std::size_t i = 0; i < node->size; ++i) {
             if (!node->is_leaf)
                 result += to_string(node->children[i], sep) + sep;
 
             result += std::to_string(node->entries[i].key);
 
-            if (i < node->entry_count - 1)
+            if (i < node->size - 1)
                 result += sep;
         }
 
         if (!node->is_leaf)
-            result += sep + to_string(node->children[node->entry_count], sep);
+            result += sep + to_string(node->children[node->size], sep);
 
         return result;
     }
@@ -126,7 +120,7 @@ class BTree {
         if (node == nullptr)
             return;
 
-        for (std::size_t i = 0; i < node->entry_count; ++i) {
+        for (std::size_t i = 0; i < node->size; ++i) {
             auto& entry = node->entries[i];
 
             if (self.cmp(begin, entry.key))
@@ -136,7 +130,7 @@ class BTree {
                 out.push({&entry.key, &entry.value});
         }
 
-        range_search(self, node->children[node->entry_count], out, begin, end);
+        range_search(self, node->children[node->size], out, begin, end);
     }
 
     void swap(BTree& other) noexcept {
@@ -149,10 +143,10 @@ class BTree {
             return std::pair{Entry(std::move(key), std::move(value)), nullptr};
 
         std::size_t i = 0;
-        while (i < node->entry_count && cmp(node->entries[i].key, key))
+        while (i < node->size && cmp(node->entries[i].key, key))
             ++i;
 
-        if (i < node->entry_count && node->entries[i].key == key) {
+        if (i < node->size && node->entries[i].key == key) {
             node->entries[i].value = std::move(value);
             return false;
         }
@@ -164,17 +158,17 @@ class BTree {
 
         auto& [new_entry, new_child] = std::get<std::pair<Entry, Node*>>(result);
 
-        if (node->entry_count < M - 1) {
-            node->children[node->entry_count + 1] = node->children[node->entry_count];
+        if (node->size < M - 1) {
+            node->children[node->size + 1] = node->children[node->size];
 
-            for (std::size_t j = node->entry_count; j > i; --j) {
+            for (std::size_t j = node->size; j > i; --j) {
                 node->entries[j] = std::move(node->entries[j - 1]);
                 node->children[j] = node->children[j - 1];
             }
 
             node->entries[i] = std::move(new_entry);
             node->children[i] = new_child;
-            ++node->entry_count;
+            ++node->size;
 
             return true;
         }
@@ -186,7 +180,7 @@ class BTree {
 
         for (std::size_t j = 0; j < M; ++j) {
             if (new_entry_pending &&
-                (e >= node->entry_count || cmp(new_entry.key, node->entries[e].key))) {
+                (e >= node->size || cmp(new_entry.key, node->entries[e].key))) {
                 entries_tmp[j] = std::move(new_entry);
                 children_tmp[j] = new_child;
                 new_entry_pending = false;
@@ -205,28 +199,157 @@ class BTree {
         Node* const left_split = new Node();
         left_split->is_leaf = node->is_leaf;
 
-        left_split->entry_count = mid;
-        node->entry_count = M - 1 - mid;
+        left_split->size = mid;
+        node->size = M - 1 - mid;
 
-        for (std::size_t i = 0; i < left_split->entry_count; ++i) {
+        for (std::size_t i = 0; i < left_split->size; ++i) {
             left_split->entries[i] = std::move(entries_tmp[i]);
             left_split->children[i] = children_tmp[i];
         }
 
-        left_split->children[left_split->entry_count] = children_tmp[mid];
+        left_split->children[left_split->size] = children_tmp[mid];
 
-        for (std::size_t i = 0; i < node->entry_count; ++i) {
+        for (std::size_t i = 0; i < node->size; ++i) {
             node->entries[i] = std::move(entries_tmp[mid + 1 + i]);
             node->children[i] = children_tmp[mid + 1 + i];
         }
 
-        node->children[node->entry_count] = children_tmp[M];
+        node->children[node->size] = children_tmp[M];
 
         return std::pair{lifted, left_split};
     }
 
-    [[nodiscard]] std::size_t height(const Node* const node) const {
-        std::size_t height = 0;
+    static void merge_children(Node* const node, const std::size_t i) {
+        Node* const left = node->children[i];
+        Node* const right = node->children[i + 1];
+
+        left->entries[left->size] = std::move(node->entries[i]);
+
+        for (std::size_t k = 0; k < right->size; ++k) {
+            left->entries[left->size + 1 + k] = std::move(right->entries[k]);
+            left->children[left->size + 1 + k] = std::exchange(right->children[k], nullptr);
+        }
+
+        left->children[left->size + 1 + right->size] =
+            std::exchange(right->children[right->size], nullptr);
+
+        left->size += 1 + right->size;
+
+        delete std::exchange(node->children[i + 1], node->children[i]);
+
+        for (std::size_t k = i; k < node->size - 1; ++k) {
+            node->entries[k] = std::move(node->entries[k + 1]);
+            node->children[k] = std::exchange(node->children[k + 1], nullptr);
+        }
+
+        node->children[node->size - 1] = std::exchange(node->children[node->size], nullptr);
+
+        --node->size;
+    }
+
+    enum class DeleteResult : std::uint8_t {
+        NotDeleted,
+        JustDeleted,
+        Deleted,
+    };
+
+    DeleteResult remove(Node* const node, const K& key) {
+        if (node == nullptr)
+            return DeleteResult::NotDeleted;
+
+        std::size_t i = 0;
+        while (i < node->size && cmp(node->entries[i].key, key))
+            ++i;
+
+        const bool found_key = i < node->size && node->entries[i].key == key;
+
+        if (found_key && node->is_leaf) {
+            // Found the key!
+            for (std::size_t k = i; k < node->size - 1; ++k)
+                node->entries[k] = std::move(node->entries[k + 1]);
+
+            --node->size;
+            return DeleteResult::JustDeleted;
+        }
+
+        DeleteResult result{};
+
+        if (found_key) {
+            const Entry& succ = min_entry_by_key(node->children[i + 1]);
+            node->entries[i] = succ;
+            result = remove(node->children[i + 1], succ.key);
+        } else {
+            result = remove(node->children[i], key);
+        }
+
+        if (result != DeleteResult::JustDeleted)
+            return result;
+
+        // Only here we might need to fix properties.
+        // (This is the actually hard part)
+
+        static constexpr std::size_t MIN_KEYS = std::ceil(M / 2.0) - 1;
+
+        Node* const mid = node->children[i];
+
+        if (mid->size >= MIN_KEYS)
+            return result;
+
+        // We gotta fix node->children[i]
+
+        if (i > 0 && node->children[i - 1]->size > MIN_KEYS) {
+            // Borrow from left
+            Node* const left = node->children[i - 1];
+
+            mid->children[mid->size + 1] = std::exchange(mid->children[mid->size], nullptr);
+
+            for (std::size_t k = mid->size; k > 0; ++k) {
+                mid->entries[k] = std::move(mid->entries[k - 1]);
+                mid->children[k] = std::exchange(mid->children[k - 1], nullptr);
+            }
+
+            mid->entries[0] = std::move(node->entries[i - 1]);
+            mid->children[0] = std::exchange(left->children[left->size], nullptr);
+            node->entries[i - 1] = std::move(left->entries[left->size - 1]);
+
+            ++mid->size;
+            --left->size;
+
+            return DeleteResult::Deleted;  // We're done.
+        }
+
+        if (i < node->size && node->children[i + 1]->size > MIN_KEYS) {
+            // Borrow from right
+            Node* const right = node->children[i + 1];
+
+            mid->entries[mid->size] = std::move(node->entries[i]);
+            mid->children[mid->size + 1] = std::exchange(right->children[0], nullptr);
+            node->entries[i] = std::move(right->entries[0]);
+
+            for (std::size_t k = 0; k < right->size - 1; ++k) {
+                right->entries[k] = std::move(right->entries[k + 1]);
+                right->children[k] = std::exchange(right->children[k + 1], nullptr);
+            }
+
+            right->children[right->size - 1] = std::exchange(right->children[right->size], nullptr);
+
+            ++mid->size;
+            --right->size;
+
+            return DeleteResult::Deleted;  // We're done.
+        }
+
+        // No borrow available! We have to merge.
+        if (i < node->size)
+            merge_children(node, i);  // Merge with right
+        else
+            merge_children(node, i - 1);  // Merge with left
+
+        return DeleteResult::JustDeleted;
+    }
+
+    [[nodiscard]] std::ptrdiff_t height(const Node* const node) const {
+        std::ptrdiff_t height = -1;
         const Node* cur = node;
 
         while (cur != nullptr) {
@@ -245,24 +368,24 @@ class BTree {
         const std::size_t max_entries = M - 1;
 
         // Check entry count
-        if (node->entry_count < min_entries || node->entry_count > max_entries)
+        if (node->size < min_entries || node->size > max_entries)
             return false;
 
         // Keys must be ordered
-        for (std::size_t i = 0; i < node->entry_count - 1; ++i) {
+        for (std::size_t i = 0; i < node->size - 1; ++i) {
             if (!cmp(node->entries[i].key, node->entries[i + 1].key))
                 return false;
         }
 
         if (!node->is_leaf) {
-            // Non-leaf nodes must have entry_count + 1 children
-            for (std::size_t i = 0; i < node->entry_count + 1; ++i) {
+            // Non-leaf nodes must have size + 1 children
+            for (std::size_t i = 0; i < node->size + 1; ++i) {
                 if (node->children[i] == nullptr)
                     return false;
             }
 
             // Check properties recursively
-            for (std::size_t i = 0; i < node->entry_count + 1; ++i) {
+            for (std::size_t i = 0; i < node->size + 1; ++i) {
                 if (!check_properties(node->children[i]))
                     return false;
             }
@@ -270,34 +393,25 @@ class BTree {
             const std::size_t expected_height = height(node->children[0]);
 
             // Subtree heights should be equal
-            for (std::size_t i = 1; i < node->entry_count + 1; ++i) {
+            for (std::size_t i = 1; i < node->size + 1; ++i) {
                 // NOTE: esta llamada es costosa. Podría aplicarse un caché o algo parecido, pero no
                 // estamos considerando mucho el costo de check_properties.
                 if (height(node->children[i]) != expected_height)
                     return false;
             }
 
-            // Leftmost subtree should be less than first key
-            if (!cmp(max_key(node->children[0]), node->entries[0].key))
-                return false;
-
-            // Last key should be less than rightmost subtree
-            if (!cmp(node->entries[node->entry_count - 1].key,
-                     min_key(node->children[node->entry_count])))
-                return false;
-
             // Check that subtrees go inside the correct entries
-            for (std::size_t i = 1; i < node->entry_count; ++i) {
-                const K& min = min_key(node->children[i]);
-                const K& max = max_key(node->children[i]);
+            for (std::size_t i = 0; i < node->size; ++i) {
+                const K& key = node->entries[i].key;
+                const K& min = max_key(node->children[i]);
+                const K& max = min_key(node->children[i + 1]);
 
-                if (!cmp(node->entries[i - 1].key, min) || !cmp(max, node->entries[i].key))
+                if (!cmp(min, key) || !cmp(key, max))
                     return false;
             }
-
         } else {
             // Leaf nodes should have no children
-            for (std::size_t i = 0; i < node->entry_count + 1; ++i) {
+            for (std::size_t i = 0; i < node->size + 1; ++i) {
                 if (node->children[i] != nullptr)
                     return false;
             }
@@ -378,7 +492,7 @@ public:
         return m_size;
     }
 
-    [[nodiscard]] std::size_t height() const {
+    [[nodiscard]] std::ptrdiff_t height() const {
         return height(m_root);
     }
 
@@ -396,10 +510,10 @@ public:
         while (cur != nullptr) {
             std::size_t i = 0;
 
-            while (i < cur->entry_count && cmp(cur->entries[i].key, key))
+            while (i < cur->size && cmp(cur->entries[i].key, key))
                 ++i;
 
-            if (i < cur->entry_count && cur->entries[i].key == key)
+            if (i < cur->size && cur->entries[i].key == key)
                 return true;
 
             if (cur->is_leaf)
@@ -419,8 +533,8 @@ public:
         return get(*this, key);
     }
 
-    [[nodiscard]] std::string to_string() const {
-        return to_string(m_root);
+    [[nodiscard]] std::string to_string(const std::string& sep = ",") const {
+        return to_string(m_root, sep);
     }
 
     [[nodiscard]] Vec<std::pair<const K*, T*>> range_search(const K& begin, const K& end) {
@@ -466,7 +580,7 @@ public:
         new_root->is_leaf = new_child == nullptr;
 
         new_root->entries[0] = std::move(new_entry);
-        new_root->entry_count = 1;
+        new_root->size = 1;
 
         new_root->children[0] = new_child;
         new_root->children[1] = m_root;
@@ -477,7 +591,20 @@ public:
         return true;
     }
 
-    // bool remove(const K& key) {}
+    bool remove(const K& key) {
+        const DeleteResult result = remove(m_root, key);
+
+        if (result == DeleteResult::JustDeleted && m_root->size == 0)
+            delete std::exchange(m_root, m_root->children[0]);
+
+        const bool was_deleted =
+            result == DeleteResult::Deleted || result == DeleteResult::JustDeleted;
+
+        if (was_deleted)
+            --m_size;
+
+        return was_deleted;
+    }
 
     void clear() {
         BTree().swap(*this);
--- tests/test_btree.cpp
+++ tests/test_btree.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
+#include <functional>
 #include <utility>
 #include "btree.h"
 #include "vec.h"
@@ -42,4 +43,84 @@ TEST_CASE("insertion", "[btree]") {
 
     btree.insert(1, 1);
     btree.insert(1, 1);
+}
+
+TEST_CASE("main test", "[btree]") {
+    BTree<int, int, std::less<>, 3> btree;
+
+    int numbers[10] = {45, 75, 100, 36, 120, 70, 11, 111, 47, 114};
+
+    for (int i = 0; i < 10; i++)
+        btree.insert(numbers[i], numbers[i]);
+
+    REQUIRE(btree.check_properties());
+
+    REQUIRE(btree.to_string(" ") == "11 36 45 47 70 75 100 111 114 120");
+    REQUIRE(btree.height() == 2);
+
+    for (int i = 0; i < 10; i = i + 2)
+        REQUIRE(btree.contains_key(numbers[i]));
+
+    REQUIRE(btree.min_key() == 11);
+    REQUIRE(btree.max_key() == 120);
+
+    btree.remove(100);
+    btree.remove(111);
+    btree.remove(45);
+
+    REQUIRE(btree.check_properties());
+
+    REQUIRE(btree.to_string(" ") == "11 36 47 70 75 114 120");
+    REQUIRE(btree.height() == 1);
+
+    btree.remove(114);
+    btree.insert(40, 40);
+
+    REQUIRE(btree.to_string(" - ") == "11 - 36 - 40 - 47 - 70 - 75 - 120");
+    REQUIRE(btree.height() == 2);
+
+    btree.insert(125, 125);
+    btree.insert(115, 115);
+
+    REQUIRE(btree.max_key() == 125);
+    REQUIRE(btree.contains_key(47));
+
+    btree.remove(11);
+
+    REQUIRE(!btree.contains_key(11));
+    REQUIRE(btree.min_key() == 36);
+    REQUIRE(btree.size() == 8);
+
+    btree.clear();
+    REQUIRE(btree.size() == 0);
+    REQUIRE(btree.is_empty());
+    REQUIRE(btree.height() == -1);
+
+    Vec<std::pair<int, int>> elements = {
+        { 1,  1},
+        { 2,  2},
+        { 3,  3},
+        { 4,  4},
+        { 5,  5},
+        { 6,  6},
+        { 7,  7},
+        { 8,  8},
+        { 9,  9},
+        {10, 10},
+        {11, 11},
+        {12, 12},
+        {13, 13},
+        {14, 14},
+        {15, 15},
+        {16, 16},
+        {17, 17},
+        {18, 18},
+        {19, 19},
+        {20, 20}
+    };
+
+    auto btree2 = BTree<int, int, std::less<>, 4>::build_from_ordered_vector(elements);
+
+    REQUIRE(btree2.to_string(",") == "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20");
+    REQUIRE(btree2.check_properties());
 }
